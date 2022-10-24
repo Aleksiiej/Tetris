d8df2a9c2b173853eb232c419521cd81ec0ba669
Aleksander Ramut <aleksander.ramut@gmail.com>
2022-10-24 22:45:30 +0200

    .

--- Tetris/Block1.hpp
+++ Tetris/Block1.hpp
@@ -10,9 +10,6 @@ using namespace std;
 class BlockBoard;
 
 class Block1: public BaseBlock
-=======
-class Block1: public BaseBlock
->>>>>>> 3e7c217 (Class BaseBlock completed)
 {
 public:
 	Block1() = delete;
